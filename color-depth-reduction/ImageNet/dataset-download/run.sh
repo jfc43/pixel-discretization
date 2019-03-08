fe6b49a4b722b8ad7ae07ca9f98b6ef56f46103f
Jiefeng Chen <jfc43@jfchen666@gmail.com>
2019-03-08 16:53:50 -0600

    add fetch model; remove imagenet dataset download

--- color-depth-reduction/ImageNet/dataset-download/run.sh
+++
@@ -1,6 +0,0 @@
-# Replace CSV_FILE with path to dev_dataset.csv or final_dataset.csv
-CSV_FILE=final_dataset.csv
-# Replace OUTPUT_DIR with path to directory where all images should be stored
-OUTPUT_DIR=images_final
-# Download images
-python3 download_images.py --input_file=${CSV_FILE} --output_dir=${OUTPUT_DIR}
