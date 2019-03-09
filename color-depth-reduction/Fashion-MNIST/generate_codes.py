07d7c6bd3e148671ff4e8ee675e6230f3675df24
Jiefeng Chen <jfc43@jfchen666@gmail.com>
2019-03-09 15:03:45 -0600

    fix bug

--- color-depth-reduction/Fashion-MNIST/generate_codes.py
+++ color-depth-reduction/Fashion-MNIST/generate_codes.py
@@ -3,13 +3,13 @@ import json
 
 with open('config.json') as config_file:
   config = json.load(config_file)
-  
+
 k = config['k']
 codes_path = config['codes_path']
 
 codes = []
 for i in range(k):
-    codes.append(float(i)/(k-1))
+    codes.append([float(i)/(k-1)])
 codes = np.array(codes)
 
 print('# codes: %d'%codes.shape[0])
